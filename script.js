8ccb48dc500a5b372cfbcaec42bf985045840419
Markus Bille <43919485+MaxusTheOne@users.noreply.github.com>
2023-02-21 12:57:10 +0100

    Cleaned script.js added lifes

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
 //Begyndelsen af script
 
+//får cookien til at forsvinde når de bliver klikket
 function cookieClicker(cookieNum) {
   console.log("cookie" + cookieNum + " Clicked");
   document
     .querySelector("#cookie_container" + cookieNum)
-    .removeEventListener("click", window["cookieClick" + cookieNum]);
+    .removeEventListener("mousedown", window["cookieClick" + cookieNum]);
   document.querySelector("#cookie_container" + cookieNum).classList.add("pause");
   document.querySelector("#cookie_img" + cookieNum).classList.add("clicked");
 }
 
-function cookieClick1() {
-  cookieClicker(1);
-}
-function cookieClick2() {
-  cookieClicker(2);
-}
-function cookieClick3() {
-  cookieClicker(3);
-}
-
+//starter cookies op igen
 function startCookie(cookieNum) {
   console.log("cookie started");
   document.querySelector("#cookie_container" + cookieNum).classList.remove("roll");
@@ -29,7 +21,17 @@ function startCookie(cookieNum) {
 
   document
     .querySelector("#cookie_container" + cookieNum)
-    .addEventListener("click", window["cookieClick" + cookieNum]);
+    .addEventListener("mousedown", window["cookieClick" + cookieNum]);
+}
+//idk hvordan jeg gør det uden de her
+function cookieClick1() {
+  cookieClicker(1);
+}
+function cookieClick2() {
+  cookieClicker(2);
+}
+function cookieClick3() {
+  cookieClicker(3);
 }
 function startCookie1() {
   startCookie(1);
@@ -40,12 +42,16 @@ function startCookie2() {
 function startCookie3() {
   startCookie(3);
 }
-//Click listeners
-document.querySelector("#cookie_container1").addEventListener("click", cookieClick1);
-document.querySelector("#cookie_container2").addEventListener("click", cookieClick2);
-document.querySelector("#cookie_container3").addEventListener("click", cookieClick3);
+function start() {
+  //Click listeners
+  document.querySelector("#cookie_container1").addEventListener("mousedown", cookieClick1);
+  document.querySelector("#cookie_container2").addEventListener("mousedown", cookieClick2);
+  document.querySelector("#cookie_container3").addEventListener("mousedown", cookieClick3);
 
-//Animation end listeners
-document.querySelector("#cookie_container1").addEventListener("animationend", startCookie1);
-document.querySelector("#cookie_container2").addEventListener("animationend", startCookie2);
-document.querySelector("#cookie_container3").addEventListener("animationend", startCookie3);
+  //Animation end listeners
+  document.querySelector("#cookie_container1").addEventListener("animationend", startCookie1);
+  document.querySelector("#cookie_container2").addEventListener("animationend", startCookie2);
+  document.querySelector("#cookie_container3").addEventListener("animationend", startCookie3);
+}
+//start
+window.addEventListener("load", start);
